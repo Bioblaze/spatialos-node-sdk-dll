59c12fc46b005361fa5e0d7769b0b10f5f512e37
Bioblaze Payne <BioblazePayne@gmail.com>
2019-04-04 17:11:31 -0700

    Fixed the Problem. install.js will now trigger postinstall and it will properly download all the proper files, and get the nuget requirements.

--- install.js
+++ install.js
@@ -1,20 +1,21 @@
 var fs = require('fs');
 var path = require('path');
 var child_process = require('child_process');
-const http = require('http');
+const http = require('https');
 
 var nuget_path = path.join(__dirname, "nuget.exe");
 
 var StartInstall = new Promise(function(resolve, reject) {
   var file = fs.createWriteStream(nuget_path);
-  http.get('http://www.nuget.org/nuget.exe', function(response) {
-    response.on('data', function(data) {
-      file.write(data);
-    }).on('end', function() {
-      console.log("nuget downloaded.");
-      resolve();
-      file.end();
-    });
+  http.get('https://www.nuget.org/nuget.exe', function(response) { // This redirects.. LOL :P
+      http.get(response.headers.location, function(res) {
+        res.pipe(file);
+        file.on('finish', function() {
+          file.close();
+          console.log("nuget downloaded.");
+          resolve();
+        });
+      });
   });
 });
 
@@ -22,14 +23,12 @@ StartInstall.then(function() {
   var nuget = child_process.exec("nuget.exe update -Self");
   nuget.stdout.pipe(process.stdout)
   nuget.on('exit', function() {
-    console.log("nuget updating.")
-    return;
+    console.log("nuget updated.");
+    var nuget = child_process.exec("nuget.exe restore -SolutionDirectory ./");
+    nuget.stdout.pipe(process.stdout)
+    nuget.on('exit', function() {
+      console.log('nuget Completed!');
+      return;
+    });
   });
-}).then(function() {
-  var nuget = child_process.exec("nuget.exe restore -SolutionDirectory ./");
-  nuget.stdout.pipe(process.stdout)
-  nuget.on('exit', function() {
-    console.log('nuget Completed!');
-    return;
-  });
-});
+})
