4da32aa3c23096220c9252e3199b41207dc05269
Bioblaze Payne <BioblazePayne@gmail.com>
2019-04-05 17:33:03 -0700

    Testing, removed debug.

--- compile.js
+++ compile.js
@@ -2,8 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var child_process = require('child_process');
 var csproj = path.join(__dirname, "SpatialDLL", "SpatialDLL.csproj");
-console.log(require.main.paths);
-var msbuild = require(path.join(require.main.paths[0]," ../..", 'msbuild'));
+var msbuild = require(path.join(require.main.paths[1]," ../..", 'msbuild'));
 
 var StartCompile = new Promise(function(resolve, reject) {
   var build = new msbuild(function() {
