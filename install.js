431fb248011282c51b568e0ed9f8262f6528de94
Bioblaze Payne <BioblazePayne@gmail.com>
2019-04-05 18:23:27 -0700

    Testing

--- install.js
+++ install.js
@@ -2,7 +2,40 @@ var fs = require('fs');
 var path = require('path');
 var child_process = require('child_process');
 const http = require('https');
+var spawn = child_process.spawn;
 
+//Taken from Edge-js's whereis.js ~ THANK GOD FOR THEM!
+var getFromPath = function() {
+  var pathSep = process.platform === 'win32' ? ';' : ':';
+  var directories = process.env.PATH.split(pathSep);
+  for (var i = 0; i < directories.length; i++) {
+    for (var j = 0; j < arguments.length; j++) {
+      var filename = arguments[j];
+        var filePath = path.join(directories[i], filename);
+
+        if (fs.existsSync(filePath)) {
+            return filePath;
+        }
+    }
+  }
+  return null;
+}
+
+var csproj = path.join(__dirname, "SpatialDLL", "SpatialDLL.csproj");
+var StartCompile = new Promise(function(resolve, reject) {
+  var dotnet = getFromPath('dotnet', 'dotnet.exe');
+  spawn(dotnet, ['build', csproj, '--configuration', 'Release'], { stdio: 'inherit', cwd: path.resolve(__dirname) })
+  .on('close', function() {
+    resolve();
+  });
+});
+
+StartCompile.then(function() {
+  console.log("Compile Completed!");
+  return;
+});
+
+/*
 var nuget_path = path.join(__dirname, "nuget.exe");
 
 var StartInstall = new Promise(function(resolve, reject) {
@@ -32,3 +65,4 @@ StartInstall.then(function() {
     });
   });
 })
+*/
